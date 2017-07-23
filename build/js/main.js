0bf9eb2fc0c23876e737085a020347ec39735835
Dmytro Golovko <dmtrglvk@gmail.com>
2017-07-23 23:05:27 +0300

    test task done

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,40 @@
 $(document).ready(function() {
 
-    // Если в проекте используются встроенные js-плагины от Foundation, разкомментировать
-    // $(document).foundation();
+	function skillsCounter(){
+		var slillItem = $('.js-item'),
+			totalAmount = slillItem.length,
+			checkedAmount = $('input:checked', slillItem).length,
+			temp = checkedAmount/totalAmount,
+			degreeRotate = temp*180,
+			skillScore = parseInt(temp*1000),
+			scoreNumber = $('.score'),
+			rotateBox = $('.score-arrow');
+
+		var defaultState = 0;
+		function scoreCounter(item, value){
+			setTimeout(function(){
+				defaultState++;
+				if(item == scoreNumber){
+					item.text(defaultState);
+				} else {
+					item.css({
+						transform: 'rotate(' + value + 'deg)'
+					})
+				}
+				if(defaultState < value){
+					scoreCounter(item, value);
+				}
+			}, 5)
+		}
+		scoreCounter(scoreNumber, skillScore);
+		scoreCounter(rotateBox, degreeRotate);
+	}
+
+	skillsCounter();
+
+	$('.js-item input').on('change', function () {
+		skillsCounter();
+	})
+
 
 });
